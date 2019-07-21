f6a939f4ccae24d4ec5332cad0e7899ccf160701
Alex Bai <sxb1376@student.bham.ac.uk>
2019-07-21 19:46:36 +0100

    New gym environment for mahjong game learning from past game records

--- gym-mahjong/gym_mahjong/envs/mahjong_env.py
+++ gym-mahjong/gym_mahjong/envs/mahjong_env.py
@@ -1,12 +1,55 @@
 import gym
 from gym import error,spaces,utils
 from gym.utils import seeding
+from game.table import Table
+from game.player import Player
+from game.table_learning import  Table_learning
 
 class MahjongEnv(gym.Env):
+    table = None
 
+    dora_indicators = None
+
+    dealer_seat = 0
+    round_number = -1
+    round_wind_number = 0
+    count_of_riichi_sticks = 0
+    count_of_honba_sticks = 0
+
+    count_of_remaining_tiles = 0
+    count_of_players = 4
+
+    meld_was_called = False
+
+    # array of tiles in 34 format
+    revealed_tiles = None
+
+    has_open_tanyao = False
+    has_aka_dora = False
+
+
+
+    """
+    Description:
+        A Mahjong game against three tenhou-python-bot players.
+    Observation:
+
+        Num     Observation         Min     Max
+        0
+        1
+        2
+        3
+
+    """
     def __init__(self):
-        self
+        self.__init__table()
+        self,
+        self.action_space = spaces.Discrete()
+
         # TODO
+    def __init__table(self):
+        self.table = Table_learning()
+
 
     def step(self, action):
         self
---
+++ gym-mahjong/gym_mahjong/envs/mahjong_his_env.py
@@ -0,0 +1,29 @@
+import gym
+from gym import error,spaces,utils
+
+
+class MahjongHisEnv(gym.envs):
+
+    def __init__(self):
+        self
+        # TODO
+
+    def __init__table(self):
+        self
+    #     TODO
+
+    def step(self, action):
+        self
+        # TODO
+
+    def reset(self):
+        self
+        # TODO
+
+    def render(self, mode='human'):
+        self
+        # TODO
+
+    def close(self):
+        self
+        # TODO
