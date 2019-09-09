ddaf59d54bf9535ae6d3e3b2e9b78041c4acdce7
Shaoshi Bai <sxb1376@cs.bham.ac.uk>
2019-09-09 07:40:14 +0100

    Fix main

--- Main.py
+++ Main.py
@@ -1,42 +1,51 @@
-from gym_mahjong.envs.mahjong_his_env import MahjongEnv
+from utils.tenhouconnection import Connection
 import gym
 from QLearning import QLearning as QLe
-from matplotlib import pyplot as plt
-import numpy as np
+import gym_mahjong.envs.mahjong_his_env
 
-def playermoveGraph(validation,s):
-    import os
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    n = int(len(validation)/s)
-    i = 1
-    y = []
-    while i <= n:
-        y.append(np.sum(validation[(i-1)*s:i*s])/s)
-        i += 1
-    y.append(np.sum(validation[n*s:])/(len(validation)-n*s))
-    plt.plot(range(len(y)),y)
-    plt.show()
+def play():
+    env = gym.envs.make('Mahjong-v0')
+    QL = QLe(env)
+    connection = Connection(QL)
+    connection.connect()
+    auth = connection.authencate()
+    print(auth)
+    if auth:
+        connection.play()
+    else:
+        connection.end()
 
-env = gym.envs.make('Mahjong-v0')
-QL = QLe(env)
-validation = []
-for episode in range(30000):
-    observation = env.reset_()
-    while True:
-        action = QL.decision(observation)
-        observation_after, reward, finish,validate = env.step(action)
-        QL.toHistory(observation,action,reward,observation_after)
-        validation.append(validate)
-        if (episode > 200) and (episode%10 == 0):
-            QL.learn()
-            # print('Learned!')
+def train_and_play():
+    env = gym.envs.make('Mahjong-v0')
+    QL = QLe(env)
+    validation = []
+    for episode in range(300):
+        observation = env.reset_()
+        while True:
+            action = QL.decision(observation)
+            observation_after, reward, finish, validate = env.step(action)
+            QL.toHistory(observation, action, reward, observation_after)
+            validation.append(validate)
+            if (episode > 200) and (episode % 5 == 0):
+                QL.learn()
+                # print('Learned!')
 
-        observation = observation_after
+            observation = observation_after
 
-        if finish:
-            break
+            if finish:
+                break
+    connection = Connection(QL)
 
-print('done')
-QL.toGraph()
 
-playermoveGraph(np.array(validation),100)
+    connection.connect()
+    auth = connection.authencate()
+    print(auth)
+    if auth:
+        connection.play()
+    else:
+        connection.end()
+
+
+
+if __name__ == '__main__':
+    play()
